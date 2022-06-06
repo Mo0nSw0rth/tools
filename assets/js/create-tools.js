e5f59d671fc4199e5621d7d2c3412e41f33e1ebf
ShortyDev <shortydev8274@gmail.com>
2022-06-06 18:00:12 +0200

    formatting

--- assets/js/create-tools.js
+++ assets/js/create-tools.js
@@ -260,8 +260,8 @@ new ToolBuilder("Random color", "Randomly generate a color", "Random")
         let c = hexColor.substring(1);
         let rgb = parseInt(c, 16);
         let r = (rgb >> 16) & 0xff;
-        let g = (rgb >>  8) & 0xff;
-        let b = (rgb >>  0) & 0xff;
+        let g = (rgb >> 8) & 0xff;
+        let b = (rgb >> 0) & 0xff;
         let luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
         if (luma < 40)
             color.style.color = "#fff"
